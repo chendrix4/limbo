3ac3f587e8cc4041b784c9ee1a5db0622c6f9e61
chendrix4 <cdhendrix0207@gmail.com>
2019-10-31 03:27:06 -0400

    Added serial parsing from MATLAB for forward kinematics

--- Arduino/limbo/limbo.ino
+++ Arduino/limbo/limbo.ino
@@ -8,35 +8,62 @@
 */
 
 #include <Servo.h>
-#define MIN_BOUND 60
+#define MIN_BOUND 65
 #define MAX_BOUND 120
+#define NUM_CHARS 2
 
 Servo A;
 Servo B;
 Servo C;
 
-int pos = 0;    // variable to store the servo position
+int x;
+
+char id;
 
 void setup() {
+  pinMode(30, OUTPUT);
   A.attach(9);  // attaches the servo on pin 9 to the servo object
   B.attach(10);
   C.attach(11);
   Serial.begin(115200);
   delay(500);
+
+  A.write(MAX_BOUND);
+  B.write(MAX_BOUND);
+  C.write(MAX_BOUND);
+  
 }
 
 void loop() {
-  for (int c = MIN_BOUND; c < MAX_BOUND; c++) {
-    A.write(c);
-    B.write(c);
-    C.write(c);
-    delay(20);
+
+  // Get Servo ID
+  if (Serial.available() > 0) {
+    id = Serial.read();
+
+    // Get Commanded Angle
+    if (Serial.available() > 0) {
+      x = Serial.parseInt();
+
+      // Skip x's that are zero, do some error bounding
+      if (x != 0) {
+        x = min(max(x, MIN_BOUND), MAX_BOUND);
+
+        // Write to servo
+        switch (id) {
+          case 'A':
+            A.write(x);
+            break;
+          case 'B':
+            B.write(x);
+            break;
+          case 'C':
+            C.write(x);
+            break;
+        }
+      }
+      
+    }
   }
-  for (int c = MAX_BOUND; c > MIN_BOUND; c--) {
-    A.write(c);
-    B.write(c);
-    C.write(c);
-    delay(20);
-  }
+
 }
 

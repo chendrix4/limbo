e4e997c09bf002660713cff8f170eb2d442b991e
chendrix4 <cdhendrix0207@gmail.com>
2019-11-07 21:33:08 -0500

    Demo code for 10/31

--- Arduino/limbo/limbo.ino
+++ Arduino/limbo/limbo.ino
@@ -1,12 +1,3 @@
-/* Sweep
- by BARRAGAN <http://barraganstudio.com>
- This example code is in the public domain.
-
- modified 8 Nov 2013
- by Scott Fitzgerald
- http://www.arduino.cc/en/Tutorial/Sweep
-*/
-
 #include <Servo.h>
 #define MIN_BOUND 65
 #define MAX_BOUND 120
